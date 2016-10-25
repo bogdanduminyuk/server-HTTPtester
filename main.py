8bc852768f36f035e88566e400ca6dafddbea325
bogdanduminyuk <77934329bogdan@gmail.com>
2016-10-25 23:43:58 +0300

    modifying gitignore. Ignoring __pycache__. Added httpConnector-package

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # coding: utf-8
-import argparse
+from argparse import ArgumentParser
+import httpConnector
 
 def initArgs():
-    parser = argparse.ArgumentParser(description="It is a program for running php-tests by using http-queries.")
+    parser = ArgumentParser(description="It is a program for running php-tests by using http-queries.")
     parser.add_argument("URL", type=str, help="url to parse")
     parser.add_argument("-v", "--verbose", action="store_true", help="The argument is used to enable logging in console")
     return parser.parse_args()
